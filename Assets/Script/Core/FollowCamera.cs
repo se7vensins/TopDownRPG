9ed5840421305fe93a4d8174017242a8b2d7f03f
se7vensins <vincenzoclavel88@gmail.com>
2019-08-29 11:31:12 +0100

    Initial commit

---
+++ Assets/Script/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    void LateUpdate()
+    {
+        transform.position = _target.position; 
+    }
+}
---
+++ Assets/Script/Movement/Mover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    Ray lastRay;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnim();
+    }
+
+    public void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    private void UpdateAnim()
+    {
+        Vector3 _velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 _localVelocity = transform.InverseTransformDirection(_velocity);
+        float _speed = _localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", _speed);
+    }
+}
